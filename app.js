0d3333dec94d0e62d12df38afbe31280aff59935
Abby <abbyrthornton@icloud.com>
2022-10-29 10:43:06 -0400

    wrote start of API

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 //this is our js file
+const express = require('express');
+
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(cors({
+    origin: '*'
+}));
+
+app.use(bodyParser.urlencoded({
+    entended:true
+}))
+
+app.use(express.json());
+
+app.get('/',(req, res) => {
+    res.send('accessing api');
+});
+
+
+
+const port = process.env.PORT || 3001
+app.listen(port, () => console.log(`listening on ${port}`))
+
+
